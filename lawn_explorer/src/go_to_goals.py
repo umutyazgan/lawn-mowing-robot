a1cfec2acedc0559918dd5b179c62e8af281c8f7
umutyazgan <umut.yaz@gmail.com>
2018-12-23 17:37:15 +0300

    Redesign world and create better map, modify go_to_goals.py to seek better calculated goals

--- lawn_explorer/src/go_to_goals.py
+++ lawn_explorer/src/go_to_goals.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 """
 Copyright (c) 2015, Mark Silliman
 All rights reserved.
@@ -10,9 +11,6 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-# TurtleBot must have minimal.launch & amcl_demo.launch
-# running prior to starting this script
-# For simulation: launch gazebo world & amcl_demo prior to run this script
 
 import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
@@ -26,15 +24,12 @@ class GoToPose():
 
         self.goal_sent = False
 
-        # What to do if shut down (e.g. Ctrl-C or failure)
         rospy.on_shutdown(self.shutdown)
 
-        # Tell the action client that we want to spin a thread by default
         self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         rospy.loginfo("Wait for the action server to come up")
 
-        # Allow up to 5 seconds for the action server to come up
-        self.move_base.wait_for_server(rospy.Duration(5))
+        self.move_base.wait_for_server(rospy.Duration(3))
 
     def goto(self, pos, quat):
 
@@ -50,7 +45,7 @@ class GoToPose():
         self.move_base.send_goal(goal)
 
         # Allow TurtleBot up to 60 seconds to complete task
-        success = self.move_base.wait_for_result(rospy.Duration(60))
+        success = self.move_base.wait_for_result(rospy.Duration(20))
 
         state = self.move_base.get_state()
         result = False
@@ -71,14 +66,17 @@ class GoToPose():
         rospy.sleep(1)
 
 
-goal_poses = [{'x': 1, 'y': 1},
-              {'x': -2, 'y': 0},
-              {'x': -3, 'y': -5},
-              {'x': -4, 'y': 3},
-              {'x': -3, 'y': 6},
-              {'x': -1, 'y': -3},
-              {'x': -3, 'y': -4},
-              {'x': -5, 'y': 3}]
+goal_poses = [{'x': -2.95, 'y': -0.45},{'x': -4.4, 'y': -1.1},{'x': -5.25, 'y': 0.5},{'x': -6.7, 'y': 5.3},{'x': -9.25, 'y': 4.35},{'x': -11.15, 'y': 3.75},
+              {'x': -13.05, 'y': 3.1},{'x': -14.35, 'y': 2.05},{'x': -6.75, 'y': 4.65},{'x': -6.65, 'y': 4.25},{'x': -14.0, 'y': 1.55},{'x': -14.0, 'y': 1.15},
+              {'x': -6.3, 'y': 3.9},{'x': -9.95, 'y': 2.5},{'x': -8.65, 'y': 2.25},{'x': -9.7, 'y': 1.14},{'x': -8.4, 'y': 1.3},{'x': -9.3, 'y': 0.35},
+              {'x': -5.65, 'y': 1.65},{'x': -12.95, 'y': -1.65},{'x': -12.75, 'y': -2.15},{'x': -5.25, 'y': 0.85},{'x': -5.85, 'y':-0.15},{'x': -9.0, 'y': -1.55},
+              {'x': -8.6, 'y': -2.2},{'x': -10.85, 'y': -2.35},{'x': -12.45, 'y':-3.05},{'x': -12.25, 'y':-3.95},{'x': -4.45, 'y': -0.75},{'x': -4.05, 'y': -1.65},
+              {'x': -12.25, 'y':-5.15},{'x': -12.0, 'y': -6.0},{'x': -2.35, 'y': -2.2},{'x': -2.0, 'y': -3.05},{'x': -11.2, 'y':-6.85},{'x': -7.2, 'y':-5.4},
+              {'x': -5.8, 'y': -5.15},{'x': -1.75, 'y':-3.45},{'x': -4.9, 'y':-5.35},{'x': -7.65, 'y':-6.35},{'x': -7.4, 'y': -7.0},{'x': -4.55, 'y': -6.1},
+              {'x': -4.5, 'y':-7.0},{'x': -7.05, 'y':-8.1},{'x': -10.35, 'y':-7.6},{'x': -10.85, 'y':-7.6},{'x': -10.01, 'y':-10.05},{'x': -0.8, 'y':-6.45},
+              {'x': -0.4, 'y':-7.3},{'x': -10.25, 'y':-10.7},{'x': -10.0, 'y':-11.4},{'x': -5.55, 'y':-10.05},{'x': -4.05, 'y':-9.3},{'x': -0.15, 'y':-7.9},{'x': -0.12, 'y':-8.8},
+              {'x': -0.65, 'y':-10.55},{'x': -2.95, 'y':-10.2},{'x': -6.05, 'y':-11.3},{'x': -5.85, 'y':-11.95},{'x': -2.95, 'y':-10.9},{'x': -2.65, 'y':-10.8},{'x': -2.3, 'y':-13.0},
+              {'x': -8.25, 'y':-13.9},{'x': -9.2, 'y':-12.2},{'x': -8.45, 'y':-14.8},{'x': -0.85, 'y':-11.55},{'x': -3.0, 'y':-0.5}]
 
 if __name__ == '__main__':
     for goal in goal_poses:
